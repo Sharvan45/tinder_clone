91a3b75f180cf5afa1e4d469e4f331c1722fdbfa
[0x7FFFD0431290] ANOMALY: meaningless REX prefix used [0x7FFFD0446A10] ANOMALY: meaningless REX prefix used [0x7FFFD30A17D0] ANOMALY: use of REX.w is meaningless (default operand size is 64) [0x7FFFD30C7C50] ANOMALY: use of REX.w is meaningless (default operand size is 64) [0x7FFFD31A5920] ANOMALY: meaningless REX prefix used Saravanakumar <[0x7FFFD0431290] ANOMALY: meaningless REX prefix used [0x7FFFD0446A10] ANOMALY: meaningless REX prefix used [0x7FFFD30A17D0] ANOMALY: use of REX.w is meaningless (default operand size is 64) [0x7FFFD30C7C50] ANOMALY: use of REX.w is meaningless (default operand size is 64) [0x7FFFD31A5920] ANOMALY: meaningless REX prefix used sharvan7@users.noreply.github.com>
2022-07-14 21:55:27 +0530

    june 14 changes

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,23 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import { IconButton } from "@material-ui/core";
+import ForumIcon from "@material-ui/icons/Forum";
 
+const buttonProps = { fontSize: "large", className: "header_icon" };
 function Header() {
   return (
     <div className="header">
-      <h2>I am header</h2>
       <IconButton>
-        <PersonIcon fontSize="large" className="header_icon" />
+        <PersonIcon {...buttonProps} />
       </IconButton>
-      <img 
-      className="header_logo"
-      src='./Tinder_logo_PNG3.png'
-      alt=""
+      <img
+        className="header_logo"
+        src="./Tinder_logo_PNG3.png"
+        alt="not available"
       />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header_icon" />
+      </IconButton>
     </div>
   );
 }
